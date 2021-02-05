783fcbccff2689ea224de907227dcc4fa84bb724
Andre Saad <andr.saad27@gmail.com>
2021-02-04 20:02:20 -0500

    look

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+from __future__ import division
+from codecs import open
+from sklearn.datasets import load_files
+import pandas as pd
+
 # COMP 472
 # WINTER 2021
 
@@ -6,34 +11,27 @@
 # Andre Saad      - xxxxxxxx
 
 
-from sklearn.datasets import load_files
-import pandas as pd
-
 
 
 # ---------------- Task 0 --------------------- #
+def read_documents(doc_file):
+    docs = []
+    labels = []
+    with open(doc_file, encoding='utf-8') as f:
+        for line in f:
+            words = line.strip().split()
+            docs.append(words[3:])
+            labels.append(words[1])
+    return docs, labels
 
-#  Counting Total number of lines in the file
-fname = "all_sentiment_shuffled.txt"
-count = 0
-with open(fname, encoding="utf8") as f:
-    for line in f:
-        count += 1
 
-print("Total number of lines is:", count)
-split_point = int(0.80*count)
+all_docs, all_labels = read_documents('all_sentiment_shuffled.txt')
 
-# Loops through each line of the file, splitting the line with a " "
-f = open("all_sentiment_shuffled.txt", encoding="utf8")
-review = ""
-for line in f:
-  fields = line.split(" ") 
-  topic = fields[0]
-  sentiment = fields[1]
-  identifier = fields[2]
-  review = fields[3] + " " + fields[4] + fields[5] # Something needs to be added here so that after the 3rd space it reads the remaining of the line as one field.
-
-  print(topic + "\t" + sentiment + "\t" + identifier + "\t" + review)
+split_point = int(0.80*len(all_docs))
+train_docs = all_docs[:split_point]
+train_labels = all_labels[:split_point]
+eval_docs = all_docs[split_point:]
+eval_labels = all_labels[split_point:]
 
 
 # ---------------- Task 1 --------------------- #
