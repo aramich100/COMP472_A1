11ad7f4d6afa22e69bc5626c25d673cb84e5ce9a
Azoies1 <thomasle98@outlook.com>
2021-02-04 19:57:26 -0500

    Completed read_documents function for reading the textfile

--- main.py
+++ main.py
@@ -14,26 +14,45 @@ import pandas as pd
 # ---------------- Task 0 --------------------- #
 
 #  Counting Total number of lines in the file
-fname = "all_sentiment_shuffled.txt"
-count = 0
-with open(fname, encoding="utf8") as f:
-    for line in f:
-        count += 1
+# fname = "all_sentiment_shuffled.txt"
+# count = 0
+# with open(fname, encoding="utf8") as f:
+#     for line in f:
+#         count += 1
+#
+# print("Total number of lines is:", count)
+# split_point = int(0.80*count)
+#
+# # Loops through each line of the file, splitting the line with a " "
+# f = open("all_sentiment_shuffled.txt", encoding="utf8")
+# review = ""
+# for line in f:
+#   fields = line.split(" ")
+#   topic = fields[0]
+#   sentiment = fields[1]
+#   identifier = fields[2]
+#   review = fields[3] + " " + fields[4] + fields[5] # Something needs to be added here so that after the 3rd space it reads the remaining of the line as one field.
+#
+#   print(topic + "\t" + sentiment + "\t" + identifier + "\t" + review)
+def read_documents (docName):
+    docs = []
+    label = []
+    with open(docName, encoding="utf8") as f:
+        for line in f:
+            split = line.split()
+            label.append(split[1])
+            docs.append(' '.join(split[3:]))
+    return docs, label
 
-print("Total number of lines is:", count)
-split_point = int(0.80*count)
+all_docs, all_labels = read_documents ('all_sentiment_shuffled.txt')
+split_point = int(0.80*len(all_docs))
+train_docs = all_docs[:split_point]
+train_labels = all_labels[:split_point]
+eval_docs = all_docs[split_point:]
+eval_labels = all_labels[split_point:]
 
-# Loops through each line of the file, splitting the line with a " "
-f = open("all_sentiment_shuffled.txt", encoding="utf8")
-review = ""
-for line in f:
-  fields = line.split(" ") 
-  topic = fields[0]
-  sentiment = fields[1]
-  identifier = fields[2]
-  review = fields[3] + " " + fields[4] + fields[5] # Something needs to be added here so that after the 3rd space it reads the remaining of the line as one field.
+print(all_docs[1])
 
-  print(topic + "\t" + sentiment + "\t" + identifier + "\t" + review)
 
 
 # ---------------- Task 1 --------------------- #
