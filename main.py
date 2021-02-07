2d54461be8e0749ab627ee65a95b42c6666c00c2
Andre Saad <andr.saad27@gmail.com>
2021-02-06 22:49:00 -0500

    naives

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from __future__ import division
 from codecs import open
+from collections import Counter
 from sklearn.datasets import load_files
+from sklearn import preprocessing
+from sklearn.naive_bayes import GaussianNB
+from math import sqrt
 import pandas as pd
+import matplotlib.pyplot as plt
+import re
+import pandas as pd
+import numpy as np
+
 
 # COMP 472
 # WINTER 2021
@@ -29,8 +38,32 @@ train_docs = all_docs[:split_point]
 train_labels = all_labels[:split_point]
 eval_docs = all_docs[split_point:]
 eval_labels = all_labels[split_point:]
-print(all_labels[1])
+#print(all_labels[1])
 
 
 # ---------------- Task 1 --------------------- #
+frequency = Counter()
 
+for doc in all_docs:
+    for word in doc:
+        frequency.update(word)
+print(frequency.values())
+plt.bar(frequency.keys(), frequency.values())
+plt.title("Distribution Plot")
+plt.xlabel("Word/Letter")
+plt.ylabel("Frequency")
+plt.show()
+
+# ---------------- Task 2 --------------------- #
+letter = list(frequency.keys())
+letter_array = np.array(letter)
+le = preprocessing.LabelEncoder()
+letter_encoder = le.fit(letter_array)
+print (letter_encoder)
+values = list(frequency.values())
+values_array = np.array(values)
+
+features = zip(letter_array, values_array)
+print(features)
+
+model = GaussianNB()
