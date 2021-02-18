32aad855fd3aeb3baf0b1aed3d9be517f00b8c56
Andre Saad <andr.saad27@gmail.com>
2021-02-17 23:47:16 -0500

    matrix

--- main.py
+++ main.py
@@ -1,20 +1,3 @@
-from __future__ import division
-from sklearn.datasets import load_iris
-from sklearn.model_selection import train_test_split
-from codecs import open
-from collections import Counter
-from sklearn import datasets
-from sklearn.datasets import load_files
-from sklearn import preprocessing
-from sklearn.naive_bayes import GaussianNB
-from math import sqrt
-import pandas as pd
-import matplotlib.pyplot as plt
-import re
-import pandas as pd
-import numpy as np
-
-
 # COMP 472
 # WINTER 2021
 
@@ -22,29 +5,45 @@ import numpy as np
 # Thomas Le       - 40096120
 # Andre Saad      - 40076579
 
+from sklearn.metrics import confusion_matrix
+from sklearn.datasets import make_classification
+from sklearn.svm import SVC
+from sklearn import tree
+from sklearn.datasets import load_files
+from sklearn import *
+from sklearn.feature_extraction.text import *
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import *
+from collections import Counter
+from sklearn.metrics import plot_confusion_matrix
+import matplotlib.pyplot as plt
+import re
+import pandas as pd
+import numpy
+
 
 # ---------------- Task 0 --------------------- #
-def read_documents(doc_file):
+
+def read_documents(docName):
     docs = []
-    labels = []
-    with open(doc_file, encoding='utf-8') as f:
+    label = []
+    with open(docName, encoding="utf8") as f:
         for line in f:
-            words = line.strip().split()
-            docs.append(words[3:])
-            labels.append(words[1])
-    return docs, labels
+            split_line = re.findall(r'\w+', line)
+            label.append(split_line[1])
+            docs.append(" ".join(split_line[4:]))
+    return docs, label
+
 
 all_docs, all_labels = read_documents('all_sentiment_shuffled.txt')
-
-split_point = int(0.80*len(all_docs))
+split_point = int(0.80 * len(all_docs))
 train_docs = all_docs[:split_point]
 train_labels = all_labels[:split_point]
 eval_docs = all_docs[split_point:]
 eval_labels = all_labels[split_point:]
-#print(all_labels[1])
-
 
 # ---------------- Task 1 --------------------- #
+
 frequency = Counter()
 
 # Distribution for words
@@ -52,58 +51,69 @@ frequency = Counter()
 # for doc in all_docs:
 #     frequency.update(doc)
 
-# Distribution for positive/negative
-for doc in all_labels:
-    frequency[doc] += 1
+# # Distribution for positive/negative
+# for doc in all_labels:
+#     frequency[doc] += 1
+#
+# plt.bar(frequency.keys(), frequency.values())
+# plt.title("Distribution Plot")
+# plt.xlabel("Label")
+# plt.ylabel("Frequency")
+# plt.show()
 
-plt.bar(frequency.keys(), frequency.values())
-plt.title("Distribution Plot")
-plt.xlabel("Label")
-plt.ylabel("Frequency")
-plt.show()
 # ---------------- Task 2 --------------------- #
-letter = list(frequency.keys())
-letter_array = np.array(letter)
-le = preprocessing.LabelEncoder()
-letter_encoder = le.fit_transform(letter_array)
-#print (letter_encoder)
-values = list(frequency.values())
-values_array = np.array(values)
-#print(values_array)
-values_array2 = le.fit_transform(values_array)
-print(letter_encoder)
-print(values_array)
-features = zip(letter_array, values_array)
-features_list = list(features)
 
-X, y = load_iris(return_X_y=True)
-print(X)
-print(y)
-
-newValuesArrays = letter_encoder.reshape(-1, 1)
-newValuesArrays[0] =1
-newValuesArrays[1] =2
-print(newValuesArrays)
-#newLetterEncoder = letter_encoder.reshape(-1, 1)
-#print(newLetterEncoder)
-#print(newValuesArrays)
-#print(letter_encoder)
-X_train, X_test, y_train, y_test = train_test_split(newValuesArrays, values_array, test_size=0.5, random_state=0)
-#print(X_train)
+# Naives Bayes
+gnb = MultinomialNB()
+cv = CountVectorizer(strip_accents='ascii',
+                     token_pattern=u'(?ui)\\b\\w*[a-z]+\\w*\\b', ngram_range=(-1, 1), stop_words='english')
+train_docs_Vec = cv.fit_transform(train_docs)
+gnb.fit(train_docs_Vec, train_labels)
 
 
+eval_docs_Vec = cv.transform(eval_docs)
+predicted = gnb.predict(eval_docs_Vec)
+accuracy = metrics.accuracy_score(predicted, eval_labels)
+print("NB: ", accuracy*100)
+plot_confusion_matrix(gnb, train_docs_Vec, train_labels)
+confusion_matrix(gnb, train_docs_Vec, train_labels)
+plt.show()
+f = open("NaiveBayes_all_sentiment_shuffled.txt", "w")
+outputFile1 = "Accuracy : " + str(accuracy*100)
+f.write(outputFile1)
+#f.write(matrix)
+f.close()
 
-model = GaussianNB()
-y_pred = model.fit(X_train, y_train).predict(X_test)
-print("Number of mislabeled points out of a total %d points : %d" % (X_test.shape[0], (y_test != y_pred).sum()))
+
+# Base DT
+
+clf = tree.DecisionTreeClassifier(criterion='entropy')
+clf.fit(train_docs_Vec, train_labels)
+y_pred = clf.predict(eval_docs_Vec)
+acc = metrics.accuracy_score(y_pred, eval_labels)
+print("DT: ", acc*100)
+plot_confusion_matrix(clf, train_docs_Vec, train_labels)
+plt.show()
+plt.savefig("Base_DT_all_sentiment_shuffled.pdf")
+
+f2 = open("Base_DT_all_sentiment_shuffled.pdf", "w")
+outputFile2 = "Accuracy : " + str(acc*100)
+f2.write(outputFile2)
+f2.close()
+
+# Better DT
+clfb = tree.DecisionTreeClassifier(criterion='gini', splitter='random')
+clfb.fit(train_docs_Vec, train_labels)
+y_predb = clfb.predict(eval_docs_Vec)
+accb = metrics.accuracy_score(y_predb, eval_labels)
+print("Better DT : ", accb*100)
+plot_confusion_matrix(clfb, train_docs_Vec, train_labels)
+plt.show()
+plt.savefig("Best_DT_all_sentiment_shuffled.pdf")
+
+f3 = open("Best_DT_all_sentiment_shuffled.pdf", "w")
+outputFile3 = "Accuracy : " + str(accb*100)
+f3.write(outputFile3)
+f3.close()
 
 
-#model.fit(newLetterEncoder, values_array)
-predicted = model.predict([[0, 2]])
-print(predicted)
-wine = datasets.load_wine()
-print(wine.feature_names)
-print(wine.target_names)
-wine.data.shape
-print(wine.data[0:5])
-print(wine.target)
